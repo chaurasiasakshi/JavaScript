50d5edf64d49958be861b4de73e2fce4cbc8dbf7
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-03-20 16:37:44 +0530

    basic

--- DATA TYPES/string.js
+++ DATA TYPES/string.js
@@ -1,23 +1,3 @@
-//  ----- Variables -----
-
-var a=10;  //redeclare
-let b = 20; // local scope 
-const c = 30; //initialize value  and cannot be reassigned.
-
-console.log(a)
-console.log(b)
-console.log(c)
-
-
-// ----- 7 Data Types -----
-
-// Number = 1,2,33
-// String ="Hello" , "World" , "17"
-// Boolean = true or false
-// Null - used to indicate that something is empty.
-// Undefined - it is used when a variable has not been assigned any value yet.
-// Object - collection of properties . It can hold key-value pairs.
-
 //  ----- Strings -----
 
 let fname = "sakshi"
